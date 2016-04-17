927106f62186a63e03f8af1d9f941d7ce2d6e5fb
alleghenycreative <info@alleghenycreative.com>
2016-04-17 18:57:35 -0400

    new includes

---
+++ includes/get_modules.php
@@ -0,0 +1,22 @@
+<?php
+// Read all the Ports from SQLite into a PHP array
+
+include_once("/Volumes/Macintosh HD/Users/aaroncrawford/Downloads/OpenRepeater Dev/db/database.php");
+
+$module_results = $dbConnection->query('SELECT * FROM "modules" ORDER BY "svxlinkID" ASC')or die();
+
+$module = array();
+
+while ($module_row = $module_results->fetchArray(SQLITE3_ASSOC) ) {
+    $module_key = $module_row['moduleKey'];
+    foreach($module_row as $key => $value) {
+		$module[$module_key][$key] = $value;
+	}
+}
+
+/*
+GPIOs are read in from the database and stored in a multi-dimensional array. Once this file is included, GPIO settings can be called
+by using the following syntax: $module[22]['direction'];
+*/
+//echo $module[4]['moduleKey'];
+?>
