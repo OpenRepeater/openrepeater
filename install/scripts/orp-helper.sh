533088a0ea534eaab136b4b33917043f46fdf27d
Richard Neese <r.neese@gmail.com>
2017-01-11 09:16:11 -0600

    update ne helper script

--- install/scripts/orp-helper.sh
+++ install/scripts/orp-helper.sh
@@ -1,25 +1,37 @@
 #!/bin/bash
+# Debian like server start/stop script/restart/status/enable/disable, svxlink
+# Copyright (c) 2017 - Richard, Richard Neese <kb3vgw@gmail.com>
+# Licended under GPL v2 or later
+
 SERVICE=svxlink
 
-disable() {
+disable)
 sudo service $SERVICE stop
 sudo systemctl disable svxlink.service
-}
+;;
 
-enable() {
+enable)
 sudo systemctl enable svxlink.service
 sudo service $SERVICE start 
-}
+;;
 
-start() {
+start)
 sudo service $SERVICE start
-}
+;;
 
-restart() {
+restart) 
 sudo service $SERVICE restart
-}
+;;
 
-stop() {
+stop) 
 sudo service $SERVICE stop
-}
+;;
 
+*)
+echo "Usage: $0 {start|stop|restart|enabld|disable|status}"
+exit 2
+;;
+
+status)
+sudo service $SERVICE status
+;;
