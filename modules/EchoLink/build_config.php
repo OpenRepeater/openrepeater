1ba8eb96abc2aebb6fb5df2fb30ab218b20327e2
Albert Causing <albert@getpantheon.com>
2018-09-02 07:40:32 +0800

    Adding Proxy Settings on ConfigBuilder

--- modules/EchoLink/build_config.php
+++ modules/EchoLink/build_config.php
@@ -8,8 +8,8 @@ $echolink_clean_desc = preg_replace('/\r\n?/', "\\n", trim($options['description
 $module_config_array['Module'.$cur_mod['svxlinkName']] = [
 	'NAME' => $cur_mod['svxlinkName'],
 	'ID' => $cur_mod['svxlinkID'],
-	'TIMEOUT' => $options['timeout'],				
-
+	'TIMEOUT' => $options['timeout'],
+	
 	'SERVERS' => $options['server'],
 	'CALLSIGN' => $options['callSign'],
 	'PASSWORD' => $options['password'],
@@ -20,6 +20,10 @@ $module_config_array['Module'.$cur_mod['svxlinkName']] = [
 	'LINK_IDLE_TIMEOUT' => $options['idle_timeout'],
 	'DESCRIPTION' => $echolink_clean_desc,
 	'USE_GSM_ONLY' => '1',
+
+	'PROXY_SERVER' => $options['proxy_server'],
+	'PROXY_PORT' => $options['proxy_port'],
+	'PROXY_PASSWORD' => $options['proxy_password'],	
 ];
 
 ?>
