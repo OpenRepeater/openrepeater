97ca94b7372914b0e6595ac5028dac5c7901641d
Aaron Crawford <info@alleghenycreative.com>
2018-11-22 23:10:33 -0500

    Simplification of DB class

--- includes/classes/Database.php
+++ includes/classes/Database.php
@@ -9,33 +9,90 @@ class Database {
 
 
 	###############################################
+	# Run SQL Statements
+	###############################################
+
+	// SELECT ALL - Return all values of table as nested array. Optional custom SQL.
+	public function select_all($table_name, $custom_sql) {
+		$db = new SQLite3($this->db_loc) or die('Unable to open database');
+
+		if (isset($custom_sql)) {
+			$sql = $custom_sql;			
+		} else {
+			$sql = 'SELECT * FROM "' . $table_name . '";';
+		}
+
+		// Get column names for table. 	
+		$column_results = $db->query('PRAGMA table_info("' . $table_name . '")');			
+		while ($colArray = $column_results->fetchArray()) {
+			// Set Primary Key for parent array
+			if ($colArray['pk'] == 1) { $primary_key =  $colArray['name']; }
+			$columns[] = $colArray['name'];
+		}
+
+		$result = $db->query($sql) or die('Query failed');
+	
+		// Return all data as nested associative array
+		while ($rowArray = $result->fetchArray()) {
+			$row_pk = $rowArray[$primary_key];
+			foreach($columns as $key => $value) {
+				$nested_array[$row_pk][$value] = $rowArray[$value];
+			}
+		}
+		return $nested_array;	
+	}
+
+	// SELECT KEY/VALUE PAIR - Return table as key/value associative array
+	public function select_key_value($sql, $keyCol = NULL, $valueCol = NULL) {
+		$db = new SQLite3($this->db_loc) or die('Unable to open database');
+		$result = $db->query($sql) or die('Query failed');
+		
+		// Return key/value pairs as associative array
+		while ($rowArray = $result->fetchArray()) {
+			$key = $rowArray[$keyCol];
+			$select_array[$key] = $rowArray[$valueCol];
+		}
+		return $select_array;
+	}
+
+	// SELECT SINGLE - Return single 
+	public function select_single($sql) {
+		$db = new SQLite3($this->db_loc) or die('Unable to open database');
+		$results = $db->querySingle($sql, true) or die('Query failed');
+		return $results;
+	}
+
+	// INSERT ROW
+	public function insert($sql) {
+		$db = new SQLite3($this->db_loc) or die('Unable to open database');
+		$results = $db->query($sql) or die('Query failed');
+		if ( $db->changes() > 0 ) { return true; } else { return false; }
+	}
+
+	// DELETE ROW
+	public function delete_row($sql) {
+		$db = new SQLite3($this->db_loc) or die('Unable to open database');
+		$results = $db->query($sql) or die('Query failed');
+		if ( $db->changes() > 0 ) { return true; } else { return false; }
+	}
+
+
+
+	###############################################
 	# Settings Table
 	###############################################
 
 	public function get_settings($returnSetting = 'all') {
 		if ($returnSetting == 'all') {
-			$sql_query = 'SELECT * FROM settings';
-		} else {
-			$sql_query = "SELECT * FROM settings WHERE keyID = '".$returnSetting."'";
-		}
-
-		$db = new SQLite3($this->db_loc) or die('Unable to open database');
-		$result = $db->query($sql_query) or die('Query failed');
-		
-		if ($returnSetting == 'all') {
 			// Return ALL settings as associative array
-			while ($row = $result->fetchArray()) {
-				$key = $row['keyID'];
-				$settings[$key] = $row['value'];
-			}
-			$db->close();
-			return $settings;
-
+			$sql = "SELECT * FROM settings";
+			$results = $this->select_key_value($sql, 'keyID', 'value');
+			return $results;
 		} else {
 			// Return requested setting value as string
-			$row = $result->fetchArray();
-			$db->close();
-			return $row['value'];		
+			$sql = "SELECT * FROM settings WHERE keyID = '".$returnSetting."'";
+			$results = $this->select_single($sql, 'keyID', 'value');
+			return $results['value'];
 		}
 	}
 	
@@ -46,45 +103,29 @@ class Database {
 	###############################################
 
 	public function get_ports() {
-		$db = new SQLite3($this->db_loc) or die('Unable to open database');
-		$result = $db->query('SELECT * FROM "ports" ORDER BY "portNum" ASC') or die('Query failed');
-		
-		$ports = [];
-		while ($row = $result->fetchArray()) {			
-			$portNum = $row['portNum'];
-			foreach($row as $key => $value) {
-				$ports[$portNum][$key] = $value;
-			}
-		}
-		$db->close();
+		$sql = 'SELECT * FROM "ports" ORDER BY "portNum" ASC';
+		$ports = $this->select_all('ports', $sql);
 		return $ports;	
 	}
 
 
 	public function clear_ports_table() {
-		$db = new SQLite3($this->db_loc);
-		$db->exec("DELETE FROM ports;");
-		$db->close();
+		$sql = 'DELETE FROM ports;';
+		$delete_result = $this->delete_row($sql);
+		return $delete_result;
 	}
 
 
 	public function update_ports_table( $input_array = array() ) {
-
-		$db = new SQLite3($this->db_loc);
-
 		foreach($input_array as $portArr){  
 			$column_names = [];
 			$column_values = [];
-
 			$columns = implode(",",array_keys($portArr));
 			$escaped_values = array_values($portArr);
 			$values  = "'" . implode("','", $escaped_values) . "'";
 			$sql = "INSERT INTO ports(".$columns.") VALUES(".$values.");";
-
-			$query = $db->exec($sql);		
+			$results = $this->insert($sql);
 		}
-
-		$db->close();
 	}
 
 
@@ -94,45 +135,29 @@ class Database {
 	###############################################
 
 	public function get_gpios() {
-		$db = new SQLite3($this->db_loc) or die('Unable to open database');
-		$result = $db->query('SELECT * FROM "gpio_pins" ORDER BY "gpio_num" ASC') or die('Query failed');
-		
-		$gpio = [];
-		while ($row = $result->fetchArray()) {			
-		    $pin_number = $row['gpio_num'];
-		    foreach($row as $key => $value) {
-				$gpio[$pin_number][$key] = $value;
-		     }
-		}
-
-		$db->close();
-		return $gpio;	
+		$sql = 'SELECT * FROM "gpio_pins" ORDER BY "gpio_num" ASC';
+		$gpios = $this->select_all('gpio_pins', $sql);
+		return $gpios;	
 	}
 
 
 	public function clear_gpio_table() {
-		$db = new SQLite3($this->db_loc);
-		$db->exec("DELETE FROM gpio_pins;");
-		$db->close();
+		$sql = 'DELETE FROM gpio_pins;';
+		$delete_result = $this->delete_row($sql);
+		return $delete_result;
 	}
 
 
 	public function update_gpio_table( $input_array = array() ) {
-
-		$db = new SQLite3($this->db_loc);
-
 		foreach($input_array as $gpioArr){  
 			$column_names = [];
 			$column_values = [];
-
 			$columns = implode(",",array_keys($gpioArr));
 			$escaped_values = array_values($gpioArr);
 			$values  = "'" . implode("','", $escaped_values) . "'";
 			$sql = "INSERT INTO gpio_pins(".$columns.") VALUES(".$values.");";
-
-			$query = $db->exec($sql);		
+			$results = $this->insert($sql);
 		}
-		$db->close();
 	}
 
 
@@ -142,24 +167,13 @@ class Database {
 	###############################################
 
 	public function get_modules() {
-		$db = new SQLite3($this->db_loc) or die('Unable to open database');
-		$result = $db->query('SELECT * FROM "modules" ORDER BY "svxlinkID" ASC') or die('Query failed');
-		
-		$module = [];
-		while ($row = $result->fetchArray()) {			
-		    $module_key = $row['moduleKey'];
-		    foreach($row as $key => $value) {
-				$module[$module_key][$key] = $value;
-			}
-		}
-
-		$db->close();
-		return $module;	
+		$sql = 'SELECT * FROM "modules" ORDER BY "svxlinkID" ASC';
+		$modules = $this->select_all('modules', $sql);
+		return $modules;	
 	}
 
 
-	public function active_module($id) {
-		$db = new SQLite3($this->db_loc);
+	public function active_module($id = NULL) {
 		if(isset($id)) {
 			// Target Module
 			$sql = "UPDATE modules SET moduleEnabled='1' WHERE moduleKey='$id'";
@@ -167,13 +181,12 @@ class Database {
 			// Deactivate ALL Modules
 			$sql = "UPDATE modules SET moduleEnabled='1'";
 		}
-		$db->exec($sql);
-		$db->close();
+		$results = $this->insert($sql);
+		return $results;
 	}
 
 
 	public function deactive_module($id = NULL) {
-		$db = new SQLite3($this->db_loc);
 		if(isset($id)) {
 			// Target Module
 			$sql = "UPDATE modules SET moduleEnabled='0' WHERE moduleKey='$id'";
@@ -181,20 +194,16 @@ class Database {
 			// Deactivate ALL Modules
 			$sql = "UPDATE modules SET moduleEnabled='0'";
 		}
-		$db->exec($sql);
-		$db->close();
+		$results = $this->insert($sql);
+		return $results;
 	}
 
 
 	public function update_preset_modules( $input_array = array() ) {
-
-		$db = new SQLite3($this->db_loc);
-
 		foreach($input_array as $moduleArray){  
 			$sql = "UPDATE modules SET moduleEnabled='1', moduleOptions='".$moduleArray['moduleOptions']."' WHERE moduleName='".$moduleArray['moduleName']."';";
-			$query = $db->exec($sql);
+			$results = $this->insert($sql);
 		}
-		$db->close();
 	}
 
 
@@ -205,15 +214,8 @@ class Database {
 
 	// Read all the CTCSS Tones from SQLite into a PHP array
 	public function get_ctcss() {
-		$db = new SQLite3($this->db_loc) or die('Unable to open database');
-		$result = $db->query('SELECT * FROM "ctcss" ORDER BY "toneFreqHz" ASC') or die('Query failed');
-		
-		$ctcss = [];
-		while ($row = $result->fetchArray()) {			
-			$ctcss[$row['toneFreqHz']] = $row['toneFreqHz'];
-		}
-
-		$db->close();
+		$sql = 'SELECT * FROM ctcss';
+		$ctcss = $this->select_key_value($sql, 'toneFreqHz', 'toneFreqHz');
 		return $ctcss;	
 	}
 
@@ -224,14 +226,9 @@ class Database {
 	###############################################
 
 	public function get_version() {
-		$db = new SQLite3($this->db_loc) or die('Unable to open database');
-		$result = $db->query('SELECT * FROM version_info') or die('Query failed');
-		
-		// Return requested setting value as string
-		$row = $result->fetchArray();
-		return $row['version_num'];		
-
-		$db->close();
+		$sql = 'SELECT * FROM version_info';
+		$result = $this->select_single($sql);
+		return $result['version_num'];		
 	}
 
 
@@ -259,6 +256,8 @@ class Database {
 
 	}
 
+
+
 	###############################################
 	# Import
 	###############################################
@@ -275,6 +274,8 @@ class Database {
 		// Import SQL file
 		exec('cat ' . $sql_file . ' | sqlite3 ' . $this->db_loc);
 	}
+
+
 
 	###############################################
 	# Memcache Flag
