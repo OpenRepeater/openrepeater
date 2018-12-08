9e148d8740c9b40ecdce44009b1397dadf7fb46e
Aaron Crawford <alleghenycreative@users.noreply.github.com>
2018-12-08 12:16:32 -0500

    Remove old DB reference.

--- install/dev/test.php
+++ install/dev/test.php
@@ -1,15 +1,4 @@
 <?php 
 
-include_once("/etc/openrepeater/database.php");
-
-$dbConnection->exec('DELETE FROM "gpio_pins" WHERE type = "Port";');
-
-
-$sql = 'INSERT INTO "gpio_pins" ("gpio_num","direction","active","description","type") VALUES ("101","out","low","RX GPIO Pin","Port");';
-$dbConnection->exec($sql);
-
-
-
-$dbConnection->close();
 echo "done";
 ?>
