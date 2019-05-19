69b06d12ab083ebf24abfa95c29fc9f14cb39518
Aaron Crawford <info@alleghenycreative.com>
2019-05-18 22:49:13 -0400

    Revert "Merge pull request #52 from albertcausing/2.x.x"
    This reverts commit 9e7ff3b16b84da09ee743c295964011d6b5df744, reversing
    changes made to cf7b79281569a803cf3b274daab5ed6f04a1b305.

--- modules/EchoLink/build_config.php
+++ modules/EchoLink/build_config.php
@@ -8,7 +8,8 @@ $echolink_clean_desc = preg_replace('/\r\n?/', "\\n", trim($options['description
 $module_config_array['Module'.$cur_mod['svxlinkName']] = [
 	'NAME' => $cur_mod['svxlinkName'],
 	'ID' => $cur_mod['svxlinkID'],
-	'TIMEOUT' => $options['timeout'],
+	'TIMEOUT' => $options['timeout'],				
+
 	'SERVERS' => $options['server'],
 	'CALLSIGN' => $options['callSign'],
 	'PASSWORD' => $options['password'],
@@ -19,17 +20,6 @@ $module_config_array['Module'.$cur_mod['svxlinkName']] = [
 	'LINK_IDLE_TIMEOUT' => $options['idle_timeout'],
 	'DESCRIPTION' => $echolink_clean_desc,
 	'USE_GSM_ONLY' => '1',
-
 ];
-
-if($options['proxy_server']) {
-	$module_config_array['Module'.$cur_mod['svxlinkName']]['PROXY_SERVER'] = $options['proxy_server'];
-	if($options['proxy_port']) {
-		$module_config_array['Module'.$cur_mod['svxlinkName']]['PROXY_PORT'] = $options['proxy_port'];
-	}
-	if($options['proxy_password']) {
-		$module_config_array['Module'.$cur_mod['svxlinkName']]['PROXY_PASSWORD'] = $options['proxy_password'];
-	}
-}
 
 ?>
