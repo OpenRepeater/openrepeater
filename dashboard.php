1484a3b741395766672583cc44fd6b9ab4fd847b
Aaron Crawford <info@alleghenycreative.com>
2019-07-17 23:51:02 -0400

    improve ajax on dasboard

--- dashboard.php
+++ dashboard.php
@@ -14,14 +14,13 @@ if ((!isset($_SESSION['username'])) || (!isset($_SESSION['userID']))){
 $pageTitle = "Dashboard";
 
 $customCSS = "sys_info.css"; // "file1.css, file2.css, ... "
-$customJS = "sys_info_chart_data.php, page-dashboard.js"; // "file1.js, file2.js, ... "
+$customJS = "page-dashboard.js"; // "file1.js, file2.js, ... "
 
 include('includes/header.php');
 	
 require_once('includes/classes/System.php');
 $classSystem = new System();
 	
-include('functions/ajax_system.php');
 ?>
 
 	<div id="overlay">
@@ -29,12 +28,6 @@ include('functions/ajax_system.php');
 			<span class="msg_1"></span><span class="msg_2"></span>
 		</div>
 	</div>
-
-
-			<?php 
-				$systemTime = $classSystem->system_time();
-				$systemArray = $classSystem->system_info();
-			?>
 			
 			<script type="text/javascript">
 				var timeString = "<?=str_replace("-","/",$systemTime['datetime']);?>";
@@ -47,63 +40,17 @@ include('functions/ajax_system.php');
 						<h2><i class="icon-globe"></i> Core System Info</h2>
 					</div>
 					<div class="box-content">
-
-						<div class="info_label">Hostname:</div>
-						<div class="info_value" id="host"><?php echo $systemArray['host']; ?></div>
-						<div class="info_clear"></div>
-						
-						<div class="info_label">System Time:</div>
-						<div class="info_value">
-							<span id="cur_date"><?=$systemTime['date'];?></span><br>
-							<span id="cur_time"><?=$systemTime['time'];?></span> <?=$systemTime['tz_short'];?>
-						</div>
-						<div class="info_clear"></div>
-					
-						<div class="info_label">Kernel:</div>
-						<div class="info_value" id="kernel"><?php echo $systemArray['kernel']; ?></div>
-						<div class="info_clear"></div>
-					
-						<div class="info_label">CPUs/Cores:</div>
-						<div class="info_value" id="cpu_cores"><?php echo $systemArray['cpu_cores']; ?></div>
-						<div class="info_clear"></div>
-					
-						<div class="info_label">CPU Frequency:</div>
-						<div class="info_value" id="cpu_speed"><?php echo $systemArray['cpu_speed']; ?></div>
-						<div class="info_clear"></div>
-					
-						<div class="mem_group">
-							<div>
-								<span class="bar_left">
-									<strong>CPU Load:</strong>
-								</span>
-								<span class="bar_right">
-									<span id="cpu_load"><?php echo $systemArray['cpu_load']; ?></span>
-								</span>
-							</div>
-							<div class="bar_wrap">
-								<div id="bar5" style = "width:<?php echo $systemArray['cpu_load']; ?>;"></div>
-							</div>
-						</div>
-
-
-						
-						<?php
-							// Hide on boards that don't support this
-							if ($systemArray['cpuTempF'] != '32°F') { ?>
-								<div class="info_label">CPU Temperature:</div>
-								<div class="info_value" id="cpuTempBoth"><?php echo $systemArray['cpuTempBoth']; ?></div>
-								<div class="info_clear"></div>
-							<?php	
-							} // close if
-						?>
+						<div id="system_static">
 							
-					
-						<div class="info_label">Uptime:</div>
-						<div class="info_value" id="uptime"><?php echo $systemArray['uptime']; ?></div>
-						<div class="info_clear"></div>
+						  	<center>
+							  	<h4 style="text-align: center">Loading...</h4>
+							  	<img src="theme/img/ajax-loaders/ajax-loader-1.gif" align="middle">
+						  	</center>
 
-
-
+						</div>
+						
+						<div id="system_dynamic"></div>						
+					  	
 						<hr>
 						<!-- Button triggered modal -->
 						<button class="btn btn-success" data-toggle="modal" data-target="#systemRestart"><i class="icon-refresh icon-white"></i> Restart</button>
@@ -112,31 +59,16 @@ include('functions/ajax_system.php');
 					</div>
 
 
-					<?php 
-						### Get inital SVXLink Status on page load and decide what to display below ###
-						$svxlink_status = exec_orp_helper('svxlink', 'status');
-						if ($svxlink_status == 'active') {
-							$status_string = '<span class="label label-success">Active</span>';
-							$control_btn_text = '<i class="icon-stop"></i> Stop Repeater';
-						} else if ($svxlink_status == 'failed') {
-							$status_string = '<span class="label label-warning">FAILED</span> - <a href="log.php">View Log</a>';							
-							$control_btn_text = '<i class="icon-play"></i> Start Repeater';
-						} else {
-							$status_string = '<span class="label label">Deactivated</span>';
-							$control_btn_text = '<i class="icon-play"></i> Start Repeater';
-						}
-					?>
-
 					<div class="box-header well">
 						<h2><i class="icon-retweet"></i> SVXLink</h2>
 					</div>
-					<div class="box-content">
-						<div class="info_label">SVXLink Status:</div>
-						<div id="rptStatus" class="info_value" ><?php echo $status_string; ?></div>
+					<div id="svxlink_info" class="box-content">
 
-						<div class="info_clear"></div>
-						
-						<button id="rptControlBtn" class="btn" onclick="toggleRepeaterState();"><?php echo $control_btn_text; ?></button>
+					  	<center>
+						  	<h4 style="text-align: center">Loading...</h4>
+						  	<img src="theme/img/ajax-loaders/ajax-loader-1.gif" align="middle">
+					  	</center>
+
 					</div>
 
 				</div><!--/span-->
@@ -188,76 +120,12 @@ include('functions/ajax_system.php');
 					<div class="box-header well">
 						<h2><i class="icon-tasks"></i> Memory Usage</h2>
 					</div>
-					<div class="box-content">
+					<div id="memory_info" class="box-content">
 
-						<?php $memoryArray = $classSystem->memory_usage(); ?>
-
-						<div class="mem_group">
-							<div>
-								<span class="bar_left">
-									<strong>Used: <span id="percent_used"><?php echo $memoryArray['percent_used']; ?></span>%</strong>
-								</span>
-								<span class="bar_right">
-									<strong>Free: <span id="percent_free"><?php echo $memoryArray['percent_free']; ?></span>%</strong>
-								</span>
-							</div>
-							<div class="bar_wrap ram">
-								<div id="bar1" style = "width:<?php echo $memoryArray['percent_used'] . '%'; ?>;"></div>
-							</div>
-							<div>
-								<span class="bar_left">
-									<span id="used_mem"><?php echo $memoryArray['used_mem']; ?></span>
-								</span>
-								<span class="bar_right">
-									<span id="free_mem"><?php echo $memoryArray['free_mem']; ?></span>
-								</span>
-							</div>
-						</div>						
-						<div id="total_mem">Total Memory: <span><?php echo $memoryArray['total_mem']; ?></span></div>
-
-						<hr>
-						
-						<div class="left_col">
-							<div class="mem_group">
-								<div>
-									<span class="bar_left">
-										<strong>Buffered:</strong>
-									</span>
-									<span class="bar_right">
-										<strong><span id="percent_buff"><?php echo $memoryArray['percent_buff']; ?></span>%</strong>
-									</span>
-								</div>
-								<div class="bar_wrap">
-									<div id="bar3" style = "width:<?php echo $memoryArray['percent_buff'] . '%'; ?>;"></div>
-								</div>
-								<div>
-									<span class="bar_left">
-										<span id="buffer_mem"><?php echo $memoryArray['buffer_mem']; ?></span>
-									</span>
-								</div>
-							</div>
-						</div>
-						
-						<div class="right_col">
-							<div class="mem_group">
-								<div>
-									<span class="bar_left">
-										<strong>Cached:</strong>
-									</span>
-									<span class="bar_right">
-										<strong><span id="percent_cach"><?php echo $memoryArray['percent_cach']; ?></span>%</strong>
-									</span>
-								</div>
-								<div class="bar_wrap">
-									<div id="bar4" style = "width:<?php echo $memoryArray['percent_cach'] . '%'; ?>;"></div>
-								</div>
-								<div>
-									<span class="bar_left">
-										<span id="cache_mem"><?php echo $memoryArray['cache_mem']; ?></span>
-									</span>
-								</div>
-							</div>
-						</div>
+					  	<center>
+						  	<h4 style="text-align: center">Loading...</h4>
+						  	<img src="theme/img/ajax-loaders/ajax-loader-1.gif" align="middle">
+					  	</center>
 
 						<div class="info_clear"></div>
 						
@@ -269,15 +137,13 @@ include('functions/ajax_system.php');
 					<div class="box-header well">
 						<h2><i class="icon-hdd"></i> Disk Usage</h2>
 					</div>
-					<div class="box-content">
-						<?php
-						foreach ( $classSystem->disk_usage() as $driveNum => $driveValues ) {
-							if ($driveNum > 1) { echo '<hr>'; }
-							$drive = $driveValues['mount'] . " (" . $driveValues['typex'] . ")";
-							echo '<div id="drive_label">'.$drive.'<span id="drive_size">Capacity: '.$driveValues['capacity'].'</span></div>';
-							echo '<div id="donutchart'.$driveNum.'" style="height: 300px;"></div>';
-						}
-						?>
+					<div id="disk_info" class="box-content">
+
+					  	<center>
+						  	<h4 style="text-align: center">Loading...</h4>
+						  	<img src="theme/img/ajax-loaders/ajax-loader-1.gif" align="middle">
+					  	</center>
+
  					</div>
 				</div><!--/span-->
 
--- functions/ajax_system.php
+++ functions/ajax_system.php
@@ -1,5 +1,6 @@
 <?php
-// This is part of an AJAX operation to pass data back and forth between JavaScript, PHP, and BASH (linux system). It lets ORP do things the web server normally wouldn't have privilages to do.
+// This is part of an AJAX operation to pass data back and forth between JavaScript, PHP, and BASH (linux system). It lets ORP do things the web server normally wouldn't have privilages to do. Results polled from the the System Class are returned to JavaScript in JSON format.
+
 
 // Check to make sure options are passed by JavaScript and if so, call the function and echo results
 if (!empty($_POST)) {
@@ -7,15 +8,13 @@ if (!empty($_POST)) {
 	$option = $_POST['post_option'];
 	echo exec_orp_helper($service, $option);
 }
-
 	
+
 // Function to pass and return commands from orp_helper script in BASH
 function exec_orp_helper ($opt1, $opt2) {
-// 	$command = 'sudo /usr/sbin/orp_helper ';
 	$command = 'sudo orp_helper';
 	$command .= ' ' . $opt1;
 	$command .= ' ' . $opt2;
-
 	ob_start(); 
 	passthru($command);
 	return trim(ob_get_clean());
@@ -24,13 +23,31 @@ function exec_orp_helper ($opt1, $opt2) {
 
 // Pass System Info updates back to javascript
 if( isset($_GET['update']) ) {
-	if ($_GET['update']=='info') {
-		require_once('../includes/classes/System.php');
-		$classSystem = new System();
-		$outputArray = array_merge($classSystem->system_info(),$classSystem->memory_usage());
-		
-		header("Content-Type: application/json; charset=UTF-8");
-		echo json_encode($outputArray);
+	require_once('../includes/classes/System.php');
+	$classSystem = new System();
+
+	switch ( $_GET['update'] ) {
+		case "time":
+ 			$outputArray = $classSystem->system_time();
+	        break;
+		case "systemStatic":
+ 			$outputArray = $classSystem->system_static();
+	        break;
+		case "systemDynamic":
+ 			$outputArray = $classSystem->system_dynamic();
+	        break;
+		case "svxlink":
+ 			$outputArray = $classSystem->svxlink_status();
+	        break;
+	    case "memory":
+ 			$outputArray = $classSystem->memory_usage();
+	        break;
+	    case "disk":
+ 			$outputArray = $classSystem->disk_usage();
+	        break;
 	}
+	
+	header("Content-Type: application/json; charset=UTF-8");
+	echo json_encode($outputArray);
 }
 ?>
