84202eb0aa2aeb0eb3ebd49d4569963e0e85ec85
Aaron Crawford <info@alleghenycreative.com>
2020-12-03 23:28:08 -0500

    Fix alsamixer state not being added to backup archive and some minor whitespce changes

--- includes/classes/BackupRestore.php
+++ includes/classes/BackupRestore.php
@@ -8,9 +8,9 @@ class BackupRestore {
 	private $dateString;
 	private $callsign;
 	private $orp_version;
-	
+
 	private $backupPath = "/var/www/openrepeater/backup/";
- 	private $baseDownloadPath = '/backup/'; // This will get read and rewritten in construct below
+	private $baseDownloadPath = '/backup/'; // This will get read and rewritten in construct below
 	private $archive_base_name;
 	private $backup_file_name;
 	private $archive_build_dir;
@@ -20,7 +20,7 @@ class BackupRestore {
 	private $backup_ini_file = 'backup.ini';
 	private $alsa_state_file = 'alsamixer.state';
 	private $orp_sounds_dir = '/var/www/openrepeater/sounds/';
-	
+
 	private $Database;
 	private $Modules;
 
@@ -55,13 +55,13 @@ class BackupRestore {
 	public function create_backup() {
 		// Create build directory if it doesn't exist
 		if (!file_exists($this->archive_build_dir)) {
-		    mkdir($this->archive_build_dir, 0777, true);
+			mkdir($this->archive_build_dir, 0777, true);
 		}
 
 		// Create INI file with basic info
 		$this->write_ini( $this->archive_build_dir . $this->backup_ini_file );
-		
- 		// Dump the sqlite database, specified tables only
+
+		// Dump the sqlite database, specified tables only
 		$sql_file = $this->archive_build_dir . $this->backup_db_file;
 		$this->Database->db_export( $this->db_tables, $sql_file );
 
@@ -79,17 +79,17 @@ class BackupRestore {
 			return array(
 				'msgType' => 'success',
 				'msgText' => 'Successfully created backup: <strong>' . $this->backup_file_name . '</strong>'
-			);	
+			);
 		} else {
 			return array(
 				'msgType' => 'error',
 				'msgText' => 'There was a problem creating the backup. Please try again. If the problem persists, it may be due to a permissions issue.'
-			);				
+			);
 		}
 
 	}
-	
-	
+
+
 
 
 	###############################################
@@ -107,11 +107,11 @@ class BackupRestore {
 
 		// Create Restore directory if it doesn't exist
 		if (!file_exists($this->backup_restore_dir)) {
-		    mkdir($this->backup_restore_dir, 0777, true);
+			mkdir($this->backup_restore_dir, 0777, true);
 		} else {
 			// OR if it does exist, then remove it recursively and recreate
 			$this->removeDirectory($this->backup_restore_dir);
-		    mkdir($this->backup_restore_dir, 0777, true);
+			mkdir($this->backup_restore_dir, 0777, true);
 		}
 
 		// Unpack Backup into Temp Folder
@@ -128,7 +128,7 @@ class BackupRestore {
 			$curr_orp_verion = $this->Database->get_version();
 			$ini_array = $this->read_ini( $this->backup_restore_dir . $this->backup_ini_file );
 			if ($curr_orp_verion == $ini_array['ORP_Backup']['orp_version']) {
-				$data['versionMatch'] = true;				
+				$data['versionMatch'] = true;
 			} else {
 				$data['versionMatch'] = false;
 			}
@@ -136,20 +136,20 @@ class BackupRestore {
 			$data['backup_orp_verion'] = $ini_array['ORP_Backup']['orp_version'];
 			$data['backup_callsign'] = $ini_array['ORP_Backup']['orp_callsign'];
 			$data['backup_date'] = date( "F j, Y, g:i a", strtotime( $ini_array['ORP_Backup']['backup_date'] ) );
-			
+
 		} else {
-	        $data['status'] = 'error';
+			$data['status'] = 'error';
 		}
 
-        $data['errorLevel'] = $errorLevel;
+		$data['errorLevel'] = $errorLevel;
 
-	    //returns data as JSON format
-	    echo json_encode($data);		
+		//returns data as JSON format
+		echo json_encode($data);
 	}
 
 
 	public function restore_backup() {
-		
+
 		########################################
 		# Copy Sounds into place
 
@@ -157,7 +157,7 @@ class BackupRestore {
 		$existing_identification = $this->orp_sounds_dir . 'identification/';
 		$restore_courtesy_tones = $this->backup_restore_dir . 'courtesy_tones/';
 		$restore_identification = $this->backup_restore_dir . 'identification/';
-		
+
 		// If Courtesy Tones exist in backup, then restore
 		if (file_exists($restore_courtesy_tones)) {
 			$this->removeDirectory($existing_courtesy_tones);
@@ -173,7 +173,7 @@ class BackupRestore {
 
 		########################################
 		# Restore Modules (non-core / add-ons)
-		
+
 		$restore_module_path = $this->backup_restore_dir . 'modules/';
 		
 		if (file_exists($restore_module_path)) {
@@ -182,14 +182,14 @@ class BackupRestore {
 			if(isset($non_core_modules)) {
 				foreach($non_core_modules as $mod_name => $mod_path) {
 					$this->Modules->remove_module($mod_name);
-				}				
+				}
 			}
 
 			// Restore backed up modules
 			$mod_folder_list  = glob($restore_module_path . '*', GLOB_ONLYDIR);
 			foreach($mod_folder_list as $mod_path) {
 				$mod_name = basename($mod_path);
-				exec('cp "' . $mod_path . '" "' . $this->Modules->modules_path . $mod_name . '" -R');			
+				exec('cp "' . $mod_path . '" "' . $this->Modules->modules_path . $mod_name . '" -R');
 
 				// Init module just to create symlinks. DB will get overwritten below
 				$this->Modules->initialize_module($mod_name, 0);
@@ -210,7 +210,7 @@ class BackupRestore {
 
 		// Cleanup/Delete Restore directory
 		$this->removeDirectory($this->backup_restore_dir);
-		
+
 		// Set Rebuild Flag
 		$this->Database->set_update_flag(true);
 
@@ -229,12 +229,13 @@ class BackupRestore {
 
 	public function build_archive() {
 		try	{
-		    $archive = new PharData($this->archive_build_dir . $this->archive_base_name . '.tar');
-		
-		    // Add Files/Folders to Archive
-		    $archive->addFile($this->archive_build_dir . $this->backup_db_file, $this->backup_db_file); // DB
-		    $archive->addFile($this->archive_build_dir . $this->backup_ini_file, $this->backup_ini_file); // INI
-			if (file_exists($this->alsa_state_file)) {
+			$archive = new PharData($this->archive_build_dir . $this->archive_base_name . '.tar');
+
+			// Add Files/Folders to Archive
+			$archive->addFile($this->archive_build_dir . $this->backup_db_file, $this->backup_db_file); // DB
+			$archive->addFile($this->archive_build_dir . $this->backup_ini_file, $this->backup_ini_file); // INI
+
+			if ( file_exists($this->archive_build_dir . $this->alsa_state_file) ) {
 				$archive->addFile($this->archive_build_dir . $this->alsa_state_file, $this->alsa_state_file); // alsamixer backup
 			}
 
@@ -246,28 +247,28 @@ class BackupRestore {
 			if ( isset($non_core_modules) ) {
 				$mod_build_dir = $this->archive_build_dir . 'mod_build/modules';
 				if (!file_exists($mod_build_dir)) { mkdir($mod_build_dir, 0777, true); }
-	
+
 				foreach($non_core_modules as $mod_name => $mod_path) {
-					exec('cp "' . $mod_path . '" "' . $mod_build_dir . '/' . $mod_name . '" -R');			
+					exec('cp "' . $mod_path . '" "' . $mod_build_dir . '/' . $mod_name . '" -R');
 				}
-				$archive->buildFromDirectory($this->archive_build_dir . 'mod_build/');				
+				$archive->buildFromDirectory($this->archive_build_dir . 'mod_build/');
 			}
 
 
-		    // Compress Archive
-		    $archive->compress(Phar::GZ);
-		
+			// Compress Archive
+			$archive->compress(Phar::GZ);
+
 			// Remove .tar
-		    unlink($this->archive_build_dir . $this->archive_base_name . '.tar');
+			unlink($this->archive_build_dir . $this->archive_base_name . '.tar');
 
 			// Rename Compressed format with ORP extension
 			rename($this->archive_build_dir . $this->archive_base_name . '.tar.gz', $this->backupPath . $this->archive_base_name . '.orp');
-			
+
 			// Set file name as variable for verification and display
 			$this->backup_file_name = $this->archive_base_name . '.orp';
 
 		} catch (Exception $e) {
-		    echo "Exception : " . $e;
+			echo "Exception : " . $e;
 		}
 	}
 
@@ -276,7 +277,7 @@ class BackupRestore {
 		try	{
 			// Create restore directory if it doesn't exist
 			if (!file_exists($this->backup_restore_dir)) {
-			    mkdir($this->backup_restore_dir, 0777, true);
+				mkdir($this->backup_restore_dir, 0777, true);
 			}
 
 			// Copy ORP package back to .tar.gz extension in restore directory
@@ -286,20 +287,20 @@ class BackupRestore {
 			$restore = new PharData($this->backup_restore_dir . $this->archive_base_name . '.tar.gz');
 			$restore->decompress();
 
-		    $restore = new PharData($this->backup_restore_dir . $this->archive_base_name . '.tar');
-		    $restore->extractTo($this->backup_restore_dir); // extract all files
+			$restore = new PharData($this->backup_restore_dir . $this->archive_base_name . '.tar');
+			$restore->extractTo($this->backup_restore_dir); // extract all files
 
 			// Remove .tar.gz and .tar
-		    unlink($this->backup_restore_dir . $this->archive_base_name . '.tar.gz');
-		    unlink($this->backup_restore_dir . $this->archive_base_name . '.tar');
+			unlink($this->backup_restore_dir . $this->archive_base_name . '.tar.gz');
+			unlink($this->backup_restore_dir . $this->archive_base_name . '.tar');
 
 		} catch (Exception $e) {
-		    echo "Exception : " . $e;
+			echo "Exception : " . $e;
 		}
 	}
 
 
-	
+
 	###############################################
 	# INI File Functions
 	###############################################
@@ -309,7 +310,7 @@ class BackupRestore {
 			'ORP_Backup' => [
 				'orp_version' => $this->orp_version,
 				'orp_callsign' => $this->callsign,
-				'backup_date' => $this->dateString				
+				'backup_date' => $this->dateString
 			]
 		];
 
@@ -321,9 +322,9 @@ class BackupRestore {
 				$backup_ini_contents .= $key . " = \"" . $value . "\"\n";
 			}
 		}
-		
+
 		file_put_contents($ini_file,$backup_ini_contents);
-	}	
+	}
 
 
 	public function read_ini($ini_file) {
@@ -346,19 +347,19 @@ class BackupRestore {
 		for($i=0; $i<count($fileNameArray['name']); $i++) {
 			//Get the temp file path
 			$tmpFile1 = $fileNameArray['tmp_name'][$i];
-			
+
 			$temp_ext = explode(".", $fileNameArray['name'][$i]);
 			$extension = end($temp_ext);
 
 			$currFile = $this->backupPath . str_replace(" ","_",$fileNameArray['name'][$i]);
-			
+
 
 			# Check File Size isn't too large
 			if($fileNameArray['size'][$i] > $maxFileSize){
 				return array(
 					'msgType' => 'error',
 					'msgText' => 'Sorry, but the file you tried to upload is <strong>too large</strong>.'
-				);	
+				);
 			}
 
 			# Check to see if file is allowed type
@@ -366,7 +367,7 @@ class BackupRestore {
 				return array(
 					'msgType' => 'error',
 					'msgText' => 'Sorry, but the file you tried to upload is not in a supported format. Files must end with an .orp extension.'
-				);	
+				);
 			}
 
 			# Check to see if system temp folder is writable
@@ -374,7 +375,7 @@ class BackupRestore {
 				return array(
 					'msgType' => 'error',
 					'msgText' => 'Sorry, it looks like there is a configuration issue. The system\'s temp folder is not writable'
-				);	
+				);
 			}
 
 			# Check for error reported by file array
@@ -382,7 +383,7 @@ class BackupRestore {
 				return array(
 					'msgType' => 'error',
 					'msgText' => 'There was a problem uploading the file'
-				);	
+				);
 			}
 
 			# Check to see if file already exists
@@ -390,7 +391,7 @@ class BackupRestore {
 				return array(
 					'msgType' => 'error',
 					'msgText' => 'Sorry but the file already exists.'
-				);	
+				);
 			}
 
 
@@ -403,13 +404,13 @@ class BackupRestore {
 						'msgType' => 'success',
 						'msgText' => 'Successfully uploaded the backup file to library.'
 					);	
-					
+
 				} else {
 					// Failure
 					return array(
 						'msgType' => 'error',
 						'msgText' => 'There was a problem uploading the file.'
-					);						
+					);
 				}
 				
 			}
@@ -420,7 +421,7 @@ class BackupRestore {
 		return array(
 			'msgType' => 'error',
 			'msgText' => 'Don\'t know what happened, but nothing appears to have been done.'
-		);	
+		);
 
 	}
 
@@ -431,9 +432,9 @@ class BackupRestore {
 	###############################################
 
 	public function get_backup_files() {
-	
+
 		$baseURL = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . $this->backupPath;
-		
+
 		// Read Files into 1 dimensional array
 		if ($handle = opendir($this->backupPath)) {
 				while (false !== ($file = readdir($handle))) {
@@ -441,13 +442,13 @@ class BackupRestore {
 				if ('..' === $file) continue;
 				$fileList[] = $file;
 			}
-			closedir($handle);	
+			closedir($handle);
 		}
-	
+
 		// Sort and reindex array
 		natsort($fileList);
 		$fileList = array_values($fileList);
-	
+
 		// Write into multidimensional array with clean file labels
 		foreach($fileList as $fileName) {	
 			$fileLabel = str_replace("_"," ",$fileName); //replace underscores with spaces for file labels
@@ -457,10 +458,10 @@ class BackupRestore {
 			$filePath = $this->backupPath . $fileName;
 			$fileSize = filesize($filePath);
 			$fileDate = filemtime($filePath);
-			$filesArray[] = array('fileName' => $fileName, 'fileLabel' => $fileLabel, 'downloadURL' => $downloadURL, 'filePath' => $filePath, 'fileSize' => $fileSize, 'fileDate' => $fileDate);			
+			$filesArray[] = array('fileName' => $fileName, 'fileLabel' => $fileLabel, 'downloadURL' => $downloadURL, 'filePath' => $filePath, 'fileSize' => $fileSize, 'fileDate' => $fileDate);		
 		}
-	
-		return $filesArray;
+
+	return $filesArray;
 	}
 
 
@@ -477,29 +478,27 @@ class BackupRestore {
 		if ($backupLib) {			
 			$displayHTML = '<table class="table table-striped table-condensed bootstrap-datatable">';
 			$displayHTML .= '<table class="table table-striped table-condensed bootstrap-datatable">';
-			$displayHTML .= '<thead><tr class="audio_row"><th>Name</th><th>Date</th><th>Size</th><th class="button_grp">Actions</th></tr></thead>   
-	<tbody>';
+			$displayHTML .= '<thead><tr class="audio_row"><th>Name</th><th>Date</th><th>Size</th><th class="button_grp">Actions</th></tr></thead><tbody>';
 
-
-			foreach($backupLib as $fileArray) {	
+			foreach($backupLib as $fileArray) {
 				if (!in_array($fileArray['fileName'], $hidden_array, true)) {
 					$total_dir_size = $total_dir_size + $fileArray['fileSize'];
-					
+
 					$displayHTML .= '
 					<tr id="shortIDsoundRow1" class="audio_row">
 						<td><h3>' . $fileArray['fileName'] . '</h3></td>
-						
+
 						<td class="center">' . date("F d Y H:i:s",$fileArray['fileDate']) . '</td>
-							
+
 						<td class="center">' . $this->formatSize($fileArray['fileSize']) . '</td>
 
 						<td class="button_grp">
-						
+
 							<button type="button" class="btn btn-success" data-toggle="modal" data-target="#restoreFile" onclick="restoreFile(\'' . $fileArray['fileName'] . '\'); return false;"><i class="icon-refresh icon-white"></i> Restore</button>
-						
+
 							<!-- Button triggered modal -->
 							<button class="btn" onclick="location.href=\'' . $this->baseDownloadPath . $fileArray['fileName'] . '\'"><i class="icon-download-alt"></i></button>
-			
+
 							<!-- Button triggered modal -->
 							<button type="button" class="btn btn-danger" data-toggle="modal" data-target="#deleteFile" onclick="deleteFile(\'' . $fileArray['fileName'] . '\'); return false;"><i class="icon-trash icon-white"></i></button>
 						</td>
@@ -511,8 +510,8 @@ class BackupRestore {
 			$displayHTML .= '</tbody></table>';
 
 			$displayHTML .= 'TOTAL FILE SIZE: ' . $this->formatSize($total_dir_size);
-			
-			echo $displayHTML;		
+
+			echo $displayHTML;
 
 		} else {
 			echo "no files";
@@ -527,7 +526,7 @@ class BackupRestore {
 	###############################################
 
 	public function deleteBackup($file) {
-	    unlink($this->backupPath . $file);
+		unlink($this->backupPath . $file);
 
 		if (!file_exists($this->backupPath . $file)) {
 			return array(
@@ -538,7 +537,7 @@ class BackupRestore {
 			return array(
 				'msgType' => 'error',
 				'msgText' => 'There was a problem deleting the file. Please try again. If the problem persists, it may be due to a permissions issue.'
-			);				
+			);
 		}
 	}
 
@@ -588,14 +587,14 @@ class BackupRestore {
 	###############################################
 
 	public function formatSize($bytes, $precision = 2) { 
-	    $units = array('B', 'KB', 'MB', 'GB', 'TB'); 
-	
-	    $bytes = max($bytes, 0); 
-	    $pow = floor(($bytes ? log($bytes) : 0) / log(1024)); 
-	    $pow = min($pow, count($units) - 1); 
-	    $bytes /= pow(1024, $pow);
-	
-	    return round($bytes, $precision) . ' ' . $units[$pow]; 
+		$units = array('B', 'KB', 'MB', 'GB', 'TB'); 
+
+		$bytes = max($bytes, 0); 
+		$pow = floor(($bytes ? log($bytes) : 0) / log(1024)); 
+		$pow = min($pow, count($units) - 1); 
+		$bytes /= pow(1024, $pow);
+
+		return round($bytes, $precision) . ' ' . $units[$pow]; 
 	}
 
 }
