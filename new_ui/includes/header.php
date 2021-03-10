da21036f54e1e5da478859b6902df8ca92116b13
Aaron Crawford <info@alleghenycreative.com>
2021-03-09 23:22:49 -0500

    favicon fix

--- new_ui/includes/header.php
+++ new_ui/includes/header.php
@@ -27,6 +27,7 @@ function _($input) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="noindex,nofollow">
+	<link rel="shortcut icon" href="favicon.ico">
 	  
     <title>OpenRepeater</title>
 
