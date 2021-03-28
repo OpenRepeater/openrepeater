f7cc3cdebefad959497586a4a0f578494ab32735
Aaron Crawford <info@alleghenycreative.com>
2021-03-28 00:13:37 -0400

    add SVXLink advanced fields function

--- includes/classes/SVXLink.php
+++ includes/classes/SVXLink.php
@@ -693,5 +693,55 @@ class SVXLink {
 	}
 
 
+
+	###############################################
+	# Get Advanced SVXLink Variable Options
+	###############################################
+
+	public function get_adv_svxlink_options($section = 'logic') {
+		switch ($section) {
+		case 'logic':
+			$logicCommonArray = ['MODULES', 'CALLSIGN', 'SHORT_VOICE_ID_ENABLE', 'SHORT_CW_ID_ENABLE', 'SHORT_ANNOUNCE_ENABLE', 'SHORT_ANNOUNCE_FILE', 'LONG_VOICE_ID_ENABLE', 'LONG_CW_ID_ENABLE', 'LONG_ANNOUNCE_ENABLE', 'LONG_ANNOUNCE_FILE', 'CW_AMP', 'CW_PITCH', 'CW_CPM', 'CW_WPM', 'PHONETIC_SPELLING', 'TIME_FORMAT', 'SHORT_IDENT_INTERVAL', 'LONG_IDENT_INTERVAL', 'IDENT_ONLY_AFTER_TX', 'EXEC_CMD_ON_SQL_CLOSE', 'EVENT_HANDLER', 'DEFAULT_LANG', 'RGR_SOUND_DELAY', 'REPORT_CTCSS', 'TX_CTCSS', 'MACROS', 'FX_GAIN_NORMAL', 'FX_GAIN_LOW', 'QSO_RECORDER', 'SEL5_MACRO_RANGE', 'ONLINE_CMD', 'STATE_PTY', 'DTMF_CTRL_PTY'];
+			# SETTINGS REMOVED:
+			# TYPE, RX, TX
+
+			$logicSimplexArray = ['MUTE_RX_ON_TX', 'MUTE_TX_ON_RX', 'RGR_SOUND_ALWAYS'];
+			# SETTINGS REMOVED:
+			# TYPE (same as common section)
+			
+			$logicRepeaterArray = ['NO_REPEAT', 'IDLE_TIMEOUT', 'OPEN_ON_1750', 'OPEN_ON_CTCSS', 'OPEN_ON_DTMF', 'OPEN_ON_SEL5', 'CLOSE_ON_SEL5', 'OPEN_ON_SQL', 'OPEN_ON_SQL_AFTER_RPT_CLOSE', 'OPEN_SQL_FLANK', 'IDLE_SOUND_INTERVAL', 'SQL_FLAP_SUP_MIN_TIME', 'SQL_FLAP_SUP_MAX_COUNT', 'ACTIVATE_MODULE_ON_LONG_CMD', 'IDENT_NAG_TIMEOUT', 'IDENT_NAG_MIN_TIME'];
+			# SETTINGS REMOVED:
+			# TYPE (same as common section)
+
+			sort($logicCommonArray);
+			sort($logicSimplexArray);
+			sort($logicRepeaterArray);
+			$returnOptions = '{ "Common Variables": ["' . implode('", "', $logicCommonArray) . '"], "Simplex Logic Only": ["' . implode('", "', $logicSimplexArray) . '"], "Repeater Logic Only": ["' . implode('", "', $logicRepeaterArray) . '"] }';
+
+			break;
+
+		case 'rx':
+			$receiverArray = ['AUDIO_DEV_KEEP_OPEN', 'SQL_DET', 'SQL_START_DELAY', 'SQL_DELAY', 'SQL_HANGTIME', 'SQL_EXTENDED_HANGTIME', 'SQL_EXTENDED_HANGTIME_THRESH', 'SQL_TIMEOUT', 'VOX_FILTER_DEPTH', 'VOX_THRESH', 'CTCSS_MODE', 'CTCSS_FQ', 'CTCSS_OPEN_THRESH', 'CTCSS_CLOSE_THRESH', 'CTCSS_SNR_OFFSET', 'CTCSS_BPF_LOW', 'CTCSS_BPF_HIGH', 'SERIAL_PORT', 'SERIAL_PIN', 'SERIAL_SET_PINS', 'EVDEV_DEVNAME', 'EVDEV_OPEN', 'EVDEV_CLOSE', 'GPIO_PATH', 'GPIO_SQL_PIN', 'SIGLEV_DET', 'HID_DEVICE', 'HID_SQL_PIN', 'SIGLEV_SLOPE', 'SIGLEV_OFFSET', 'SIGLEV_BOGUS_THRESH', 'TONE_SIGLEV_MAP', 'SIGLEV_OPEN_THRESH', 'SIGLEV_CLOSE_THRESH', 'SIGLEV_MIN', 'SIGLEV_MAX', 'SIGLEV_DEFAULT', 'SIGLEV_TOGGLE_INTERVAL', 'SIGLEV_RAND_INTERVAL', 'DEEMPHASIS', 'SQL_TAIL_ELIM', 'PREAMP', 'PEAK_METER', 'DTMF_DEC_TYPE', 'DTMF_MUTING', 'DTMF_HANGTIME', 'DTMF_SERIAL', 'DTMF_PTY', 'DTMF_MAX_FWD_TWIST', 'DTMF_MAX_REV_TWIST', 'DTMF_DEBUG', '1750_MUTING', 'SEL5_TYPE', 'SEL5_DEC_TYPE', 'RAW_AUDIO_UDP_DEST', 'OB_AFSK_ENABLE', 'OB_AFSK_VOICE_GAIN', 'IB_AFSK_ENABLE', 'CTRL_PTY'];
+			# SETTINGS REMOVED:
+			# TYPE, AUDIO_DEV, AUDIO_CHANNEL
+
+			sort($receiverArray);
+			$returnOptions = '["' . implode('", "', $receiverArray) . '"]';
+			break;
+
+		case 'tx':
+			$transmitterArray = ['AUDIO_DEV_KEEP_OPEN', 'PTT_TYPE', 'PTT_PORT', 'PTT_PIN', 'GPIO_PATH', 'PTT_PTY', 'HID_DEVICE', 'HID_PTT_PIN', 'SERIAL_SET_PINS', 'PTT_HANGTIME', 'TIMEOUT', 'TX_DELAY', 'CTCSS_FQ', 'CTCSS_LEVEL', 'PREEMPHASIS', 'DTMF_TONE_LENGTH', 'DTMF_TONE_SPACING', 'DTMF_DIGIT_PWR', 'TONE_SIGLEV_MAP', 'TONE_SIGLEV_LEVEL', 'MASTER_GAIN', 'OB_AFSK_ENABLE', 'OB_AFSK_VOICE_GAIN', 'OB_AFSK_LEVEL', 'OB_AFSK_TX_DELAY', 'IB_AFSK_ENABLE', 'IB_AFSK_LEVEL', 'IB_AFSK_TX_DELAY', 'CTRL_PTY'];
+			# SETTINGS REMOVED:
+			# TYPE, AUDIO_DEV, AUDIO_CHANNEL
+			
+			sort($transmitterArray);
+			$returnOptions = '["' . implode('", "', $transmitterArray) . '"]';
+			break;
+		}
+		
+		return $returnOptions;
+		
+	}
+
 }
 ?>
