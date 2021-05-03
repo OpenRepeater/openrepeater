d423a07fb7a17ff804ac7f3e729ff2fc5feed576
Aaron Crawford <info@alleghenycreative.com>
2021-05-02 23:13:12 -0400

    remove location section from dev_ui

--- dev_ui/index.php
+++ dev_ui/index.php
@@ -10,12 +10,14 @@ if ((!isset($_SESSION['username'])) || (!isset($_SESSION['userID']))){
 
 <?php include('header.php'); ?>
 
+<h3 style="color:red;">Note: sections that are no longer available in the dev_ui indicate that the function is fully available within the new 3.0 UI with read/write capabilities.</h3>
+
 <ul>
 <li><a href="dbupdate.php">Update Database Structure</a> - If you have the old DB structure, this should fix it. It will check for certain fields and if they don't exist, only then will it make the modifications to the database. This is typically a run once script.</li>
 <li><a href="ports_add.php">Add Special Ports</a> - This is primarily to add new port types that are not supported by the current UI. This would include hidraw devices (i.e. the DMK URI or USB RIM Lite) or USB-to-Serial adapters for control of PTT and COS. You MUST edit the array in this file before you run it. After You run it, do not update ports via the main UI.</li>
 <li><a href="ports_advanced.php">Advanced SVXLink Settings</a> - This provides a means to add other SVXLink settings for the Logic, RX, and TX sections. This page will allow you to view/add/remove settings for those sections on a port by port basis. These settings will be stored in the port options in the database and available upon rebuild. They will also be supported by backup/restore.</li>
 <li><a href="linkGroups.php">Add/Update LinkGroup Settings</a> - This is for adding settings for the LinkGroups to the DB You MUST edit this file first.</li>
-<li><a href="location.php">Add Location Section</a> - This is for adding settings for the location information to share with Echolink & APRS for proper reporting.</li>
+<li><s>Add Location Section</s></li>
 <li><a href="macros.php">Add Macros Section</a> - This adds macro settings into the database from an array. You MUST edit this file first.</li>
 <li><a href="send_dtmf.php">Send DTMF Commands</a> - This is an AJAX input that allows sending DTMF to SVXLink logic cores utilizing PTYs.</li>
 </ul>
