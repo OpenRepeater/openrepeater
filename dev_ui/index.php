f74717cdf40dfac7e2d275c412c9f86f8416be6f
Aaron Crawford <info@alleghenycreative.com>
2022-10-24 12:37:13 -0400

    Remove features from Dev UI that have been implemented in New UI

--- dev_ui/index.php
+++ dev_ui/index.php
@@ -14,9 +14,9 @@ if ((!isset($_SESSION['username'])) || (!isset($_SESSION['userID']))){
 
 <ul>
 <li><a href="dbupdate.php">Update Database Structure</a> - If you have the old DB structure, this should fix it. It will check for certain fields and if they don't exist, only then will it make the modifications to the database. This is typically a run once script.</li>
-<li><a href="ports_add.php">Add Special Ports</a> - This is primarily to add new port types that are not supported by the current UI. This would include hidraw devices (i.e. the DMK URI or USB RIM Lite) or USB-to-Serial adapters for control of PTT and COS. You MUST edit the array in this file before you run it. After You run it, do not update ports via the main UI.</li>
-<li><a href="ports_advanced.php">Advanced SVXLink Settings</a> - This provides a means to add other SVXLink settings for the Logic, RX, and TX sections. This page will allow you to view/add/remove settings for those sections on a port by port basis. These settings will be stored in the port options in the database and available upon rebuild. They will also be supported by backup/restore.</li>
-<li><a href="linkGroups.php">Add/Update LinkGroup Settings</a> - This is for adding settings for the LinkGroups to the DB You MUST edit this file first.</li>
+<li><s>Add Special Ports - This is primarily to add new port types that are not supported by the current UI. This would include hidraw devices (i.e. the DMK URI or USB RIM Lite) or USB-to-Serial adapters for control of PTT and COS. You MUST edit the array in this file before you run it. After You run it, do not update ports via the main UI.</s></li>
+<li><s>Advanced SVXLink Settings - This provides a means to add other SVXLink settings for the Logic, RX, and TX sections. This page will allow you to view/add/remove settings for those sections on a port by port basis. These settings will be stored in the port options in the database and available upon rebuild. They will also be supported by backup/restore.</s></li>
+<li><s>Add/Update LinkGroup Settings - This is for adding settings for the LinkGroups to the DB You MUST edit this file first.</s></li>
 <li><s>Add Location Section</s></li>
 <li><a href="macros.php">Add Macros Section</a> - This adds macro settings into the database from an array. You MUST edit this file first.</li>
 <li><a href="send_dtmf.php">Send DTMF Commands</a> - This is an AJAX input that allows sending DTMF to SVXLink logic cores utilizing PTYs.</li>
